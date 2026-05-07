4d20b869ab1aa36490941ba4f9d3fb52fec46830
Soraxe <vanbx@outlook.fr>
2026-05-07 10:48:54 +0200

    ajout de son

--- PFA_2026/Assets/Scripts/CosmeticSystem/ShopUIManager.cs
+++ PFA_2026/Assets/Scripts/CosmeticSystem/ShopUIManager.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class ShopUIManager : MonoBehaviour
 {
     public GameObject shopPanel;
+    
+    [Header("System")]
+    [SerializeField] SoundManager soundManager;
+    
 
     public void OpenShop()
     {
+        soundManager.UISoundPlay();
         shopPanel.SetActive(true);
     }
 
     public void CloseShop()
     {
+        soundManager.UISoundPlay();
         shopPanel.SetActive(false);
     }
 }
